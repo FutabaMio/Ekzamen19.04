a11fce84957536b91b41cf4d684d69da8fa569ec
stu-pksp119 <stu-pksp119@AMDVLA3511GW07>
2023-04-19 09:37:59 +0300

    изменена функция сортировки, добавлена функция заполнения массива

--- AgensyEkz/TouristicAgency.cs
+++ AgensyEkz/TouristicAgency.cs
@@ -11,22 +11,54 @@ namespace AgensyEkz
         public Tour[] tours;
 
         //конструктор
-        public TouristicAgency(int x)
+        public TouristicAgency(string s, int x)
         {
+            name = s;
             tours = new Tour[x];
+        }
+
+        //функция заполнения массива
+        public void Filling(Tour[] tours)
+        {
+            for(int i=0; i<tours.Length; i++)
+            {
+                Console.WriteLine("Направление поездки:");
+                tours[i].travelDirection = Console.ReadLine();
+                Console.WriteLine("Продолжительность поездки (пожалуйста придерживайтесь формата ЧЧ.ММ):");
+                tours[i].time = double.Parse(Console.ReadLine());
+                Console.WriteLine("Цена поездки:");
+                tours[i].price = double.Parse(Console.ReadLine());
+            }
         }
 
         //функция сортировки
         public void Sort(Tour[] tours)
         {
             Tour buf;
-            for(int i=0; i < tours.Length; i++)
+            for (int i = 0; i < tours.Length; i++)
             {
-                for(int j=i+1; j<tours.Length-1; j++)
+                if (tours[i].time < tours[i + 1].time)
                 {
-                    buf.
+                    buf = tours[i];
+                    tours[i] = tours[i + 1];
+                    tours[i + 1] = buf;
                 }
             }
+            for (int i = 0; i < tours.Length; i++)
+            {
+                if (tours[i].price < tours[i + 1].price)
+                {
+                    buf = tours[i];
+                    tours[i] = tours[i + 1];
+                    tours[i + 1] = buf;
+                }
+            }
+        }
+
+        //функция записи в файл
+        public void WriteToFile()
+        {
+
         }
     }
 }
