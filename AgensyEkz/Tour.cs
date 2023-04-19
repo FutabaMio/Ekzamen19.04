268c143ab92348b76368275559d9d1baa46fd34b
stu-pksp119 <stu-pksp119@AMDVLA3511GW07>
2023-04-19 08:46:12 +0300

    добавлен класс Tour, созданы поля

---
+++ AgensyEkz/Tour.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgensyEkz
+{
+    public class Tour
+    {
+        string travelDirection;
+        double time; //формат ЧЧ.ММ
+        double price;
+    }
+}
