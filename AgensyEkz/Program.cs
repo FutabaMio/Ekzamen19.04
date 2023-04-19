c04e58f8aea709f2ad6814bfd3849611641a165b
stu-pksp119 <stu-pksp119@AMDVLA3511GW07>
2023-04-19 11:50:24 +0300

    работа с функциями

--- AgensyEkz/Program.cs
+++ AgensyEkz/Program.cs
@@ -10,12 +10,69 @@ namespace AgensyEkz
         static void Main(string[] args)
         {
             Console.WriteLine("Проверка работы программы о турагенстве");
-            Console.WriteLine("Пожалуйста, введите название турагенства:");
-            string name = Console.ReadLine();
             Console.WriteLine("Пожалуста, введите размер массива (количество рейсов):");
-            int.TryParse(Console.ReadLine(), out int size); 
+            int.TryParse(Console.ReadLine(), out int size);
+            TouristicAgency agency= new TouristicAgency(size);
+
             Tour[] tours = new Tour[size];
-            TouristicAgency agency= new TouristicAgency(name, size);
+            for (int i = 0; i < tours.Length; i++)
+            {
+
+                Console.WriteLine("Направление поездки:");
+                string plate = string.Empty;
+                plate = Console.ReadLine();
+                tours[i].travelDirection = plate;
+                Console.WriteLine(plate);
+                Console.WriteLine(tours[i].travelDirection);
+                Console.WriteLine("Продолжительность поездки:");
+                int.TryParse(Console.ReadLine(), out int time);
+                tours[i].time = time;
+                Console.WriteLine("Цена поездки:");
+                int.TryParse(Console.ReadLine(), out int price);
+                tours[i].price = price;
+
+            }
+
+            Tour buf;
+            for (int i = 0; i < tours.Length; i++)
+            {
+                if (tours[i].time < tours[i + 1].time)
+                {
+                    buf = tours[i];
+                    tours[i] = tours[i + 1];
+                    tours[i + 1] = buf;
+                }
+            }
+            for (int i = 0; i < tours.Length; i++)
+            {
+                if (tours[i].price < tours[i + 1].price)
+                {
+                    buf = tours[i];
+                    tours[i] = tours[i + 1];
+                    tours[i + 1] = buf;
+                }
+            }
+
+            try
+            {
+                FileStream fs = new FileStream("Sorted Tours.txt", FileMode.OpenOrCreate);
+                StreamWriter sr = new StreamWriter(fs);
+                for (int i = 0; i < tours.Length; i++)
+                {
+                    string corrected = $"Направление: {tours[i].travelDirection}, длительность: {tours[i].time}, цена: {tours[i].price} \n";
+                    sr.Write(corrected);
+                }
+            }
+            catch (IOException exc)
+            {
+                Console.WriteLine("Похоже, что-то пошло не так. Попробуйте ещё раз.");
+            }
+            catch (ObjectDisposedException exc)
+            {
+                Console.WriteLine("Похоже, что-то пошло не так. Попробуйте ещё раз.");
+            }
+
+
         }
     }
 }
--- AgensyEkz/TouristicAgency.cs
+++ AgensyEkz/TouristicAgency.cs
@@ -8,35 +8,80 @@ namespace AgensyEkz
     public class TouristicAgency
     {
         //поля
-        string name;
+        //string name;
          //int x;
         //Tour[] tours1;
 
         //конструктор
-        public TouristicAgency(string s, int size)
+        public TouristicAgency(int size)
         {
-            name = s;
+            //name = s;
             Tour[] tours = new Tour[size];
             for (int i = 0; i < tours.Length; i++)
             {
-                Console.WriteLine("Направление поездки:");
-                tours[i].travelDirection = Console.ReadLine();
-                Console.WriteLine("Продолжительность поездки:");
-                int.TryParse(Console.ReadLine(), out int time);
-                tours[i].time = time;
-                Console.WriteLine("Цена поездки:");
-                int.TryParse(Console.ReadLine(), out int price);
-                tours[i].price = price;
-
-
+                    Console.WriteLine("Направление поездки:");
+                    string plate = string.Empty;
+                    plate = Console.ReadLine();
+                    tours[i].travelDirection = plate;
+                    Console.WriteLine(plate);
+                    Console.WriteLine(tours[i].travelDirection);
+                    Console.WriteLine("Продолжительность поездки:");
+                    int.TryParse(Console.ReadLine(), out int time);
+                    tours[i].time = time;
+                    Console.WriteLine("Цена поездки:");
+                    int.TryParse(Console.ReadLine(), out int price);
+                    tours[i].price = price;
+                
             }
-            Filling(tours);
-            Sort(tours);
-            WriteToFile(tours);
+
+                Tour buf;
+                for (int i = 0; i < tours.Length; i++)
+                {
+                    if (tours[i].time < tours[i + 1].time)
+                    {
+                        buf = tours[i];
+                        tours[i] = tours[i + 1];
+                        tours[i + 1] = buf;
+                    }
+                }
+                for (int i = 0; i < tours.Length; i++)
+                {
+                    if (tours[i].price < tours[i + 1].price)
+                    {
+                        buf = tours[i];
+                        tours[i] = tours[i + 1];
+                        tours[i + 1] = buf;
+                    }
+                }
+
+                try
+                {
+                    FileStream fs = new FileStream("Sorted Tours.txt", FileMode.OpenOrCreate);
+                    StreamWriter sr = new StreamWriter(fs);
+                    for (int i = 0; i < tours.Length; i++)
+                    {
+                        string corrected = $"Направление: {tours[i].travelDirection}, длительность: {tours[i].time}, цена: {tours[i].price} \n";
+                        sr.Write(corrected);
+                    }
+                }
+                catch (IOException exc)
+                {
+                    Console.WriteLine("Похоже, что-то пошло не так. Попробуйте ещё раз.");
+                }
+                catch (ObjectDisposedException exc)
+                {
+                    Console.WriteLine("Похоже, что-то пошло не так. Попробуйте ещё раз.");
+                }
+
+            
+
+            //Filling(tours);
+            //Sort(tours);
+           // WriteToFile(tours);
         }
 
         //функция заполнения массива
-        public void Filling(Tour[] tours)
+        /*public void Filling(Tour[] tours)
         {
             for(int i=0; i<tours.Length; i++)
             {
@@ -51,7 +96,7 @@ namespace AgensyEkz
 
 
             }
-        }
+        }*/
 
         //функция сортировки
         public void Sort(Tour[] tours)
