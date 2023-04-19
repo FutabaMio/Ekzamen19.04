38f0231f4d43e63d73ce7c332b341bf314d20b7b
stu-pksp119 <stu-pksp119@AMDVLA3511GW07>
2023-04-19 09:07:22 +0300

    добавлен конструктор и заголовок функции сортировки

--- AgensyEkz/TouristicAgency.cs
+++ AgensyEkz/TouristicAgency.cs
@@ -6,8 +6,20 @@ namespace AgensyEkz
 {
     public class TouristicAgency
     {
-        
+        //поля
+        string name;
+        Tour[] tours;
 
+        //конструктор
+        public TouristicAgency(int x)
+        {
+            tours = new Tour[x];
+        }
 
+        //функция сортировки
+        public void Sort()
+        {
+            
+        }
     }
 }
