823271f5391340b260dee23659d21cbc5da0fd5e
stu-pksp119 <stu-pksp119@hq.corp.vlsu.ru>
2023-04-19 08:41:48 +0300

    Добавьте файлы проекта.

---
+++ AgensyEkz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgensyEkz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
